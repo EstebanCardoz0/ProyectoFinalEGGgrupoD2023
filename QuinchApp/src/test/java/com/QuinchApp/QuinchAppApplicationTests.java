a0fc462e9505b531b37a97f0273081b0f92f32d7
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2023-03-08 21:42:58 -0300

    primera prueba

---
+++ QuinchApp/src/test/java/com/QuinchApp/QuinchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuinchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
