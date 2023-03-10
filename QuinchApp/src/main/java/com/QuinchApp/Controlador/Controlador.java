0977855ca4f8af0b10b6a3f9126e331369b57828
Alejandro Fernandez <alejandrofernandez21.af@gmail.com>
2023-03-10 11:03:46 -0300

    primer commit

--- QuinchApp/src/main/java/com/QuinchApp/Controlador/Controlador.java
+++ QuinchApp/src/main/java/com/QuinchApp/Controlador/Controlador.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.QuinchApp.Controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Esteban
- */
 @RestController
 public class Controlador {
     
