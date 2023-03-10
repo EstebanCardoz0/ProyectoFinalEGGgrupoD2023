5eb16a7ace68930a77a4b654dd438904350b5ff7
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-03-10 10:38:05 -0300

    query buscar email

--- QuinchApp/src/main/java/com/QuinchApp/Repositorio/UsuarioRepositorio.java
+++ QuinchApp/src/main/java/com/QuinchApp/Repositorio/UsuarioRepositorio.java
@@ -2,9 +2,13 @@ package com.QuinchApp.Repositorio;
 
 import com.QuinchApp.Entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
 
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    public Usuario buscarPorEmail(@Param("email") String email);
 }
