d035f1b60de6b3682908465c8740d764da26c7ef
Gaston Reale <nashua322.r@gmail.com>
2023-03-11 17:43:54 -0300

    creacion de los repositorios

--- QuinchApp/src/main/java/com/QuinchApp/Repositorios/PropiedadRepositorio.java
+++ QuinchApp/src/main/java/com/QuinchApp/Repositorios/PropiedadRepositorio.java
@@ -1,10 +1,19 @@
 package com.QuinchApp.Repositorios;
 
 import com.QuinchApp.Entidades.Propiedad;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropiedadRepositorio extends JpaRepository<Propiedad, Integer> {
 
+    
+        @Query("SELECT pr FROM Propiedad pr WHERE pr.nombre = :nombre")
+    public Propiedad buscarPorNombre (@Param("nombre")String nombre);
+    
+    @Query("SELECT pr FROM Propiedad pr WHERE pr.propietario.nombre = :nombre")
+    public List<Propiedad> buscarPorPropietario (@Param("nombre")String nombre);
 }
--- QuinchApp/src/main/java/com/QuinchApp/Repositorios/PropietarioRepositorio.java
+++ QuinchApp/src/main/java/com/QuinchApp/Repositorios/PropietarioRepositorio.java
@@ -2,9 +2,17 @@ package com.QuinchApp.Repositorios;
 
 import com.QuinchApp.Entidades.Propietario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropietarioRepositorio extends JpaRepository<Propietario, Integer> {
+
+    @Query("SELECT p FROM Propietario p WHERE p.nombre = :nombre")
+    public Propietario buscarPorNombre(@Param("nombre") String nombre);
+
+    @Query("SELECT p FROM Propietario p WHERE p.email = :email")
+    public Propietario buscarPorEmail(@Param("email") String email);
 
 }
