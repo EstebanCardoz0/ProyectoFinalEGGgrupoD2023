1155717013eb4338c5044cf3b6475f37bf31e257
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-03-13 16:26:14 -0300

    comunicacion de registro form con controler

--- QuinchApp/src/main/java/com/QuinchApp/Controladores/UsuarioControlador.java
+++ QuinchApp/src/main/java/com/QuinchApp/Controladores/UsuarioControlador.java
@@ -2,8 +2,6 @@ package com.QuinchApp.Controladores;
 
 import com.QuinchApp.Entidades.Usuario;
 import com.QuinchApp.Servicios.UsuarioServicio;
-import java.util.Date;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,23 +27,32 @@ public class UsuarioControlador {
     }
 
     @PostMapping("/registro")
-    public String registro(@RequestParam("nombre") String nombre, @RequestParam("nombreUsuario") String nombreUsuario, 
-            @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("telefono") long telefono, 
-            @RequestParam("fechaAlta") Date fechaAlta, @RequestParam("activo") boolean activo, @RequestParam("archivo") MultipartFile archivo) throws Exception {
+    public String registro(@RequestParam("nombre") String nombre, @RequestParam("nombreUsuario") String nombreUsuario,
+            @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("password") String password2, @RequestParam("telefono") long telefono,
+            @RequestParam("archivo") MultipartFile archivo, ModelMap modelo) throws Exception {
         try {
-            usuarioServicio.registrar(nombre, nombreUsuario, email, password, telefono, archivo);
-            return "Exito";
+            usuarioServicio.registrar(nombre, nombreUsuario, email, password, password2, telefono, archivo);
+            modelo.put("exito", "El usuario fue registrado correctamente!");
         } catch (Exception exception) {
             System.out.println(exception);
-            return "Error";
+            modelo.put("nombre", nombre);
+            modelo.put("nombreUsuario", nombreUsuario);
+            modelo.put("telefono", telefono);
+            modelo.put("email", email);
+            modelo.put("password", password);
+            modelo.put("password2", password2);
+            modelo.put("archivo", archivo);
+            modelo.put("error", "Verifique que los datos hayan sido cargado correctamente y el email no este registrado");
+            return "registro";
         }
+        return "registro";
     }
-    
+
     @PostMapping("/actualizar/{id}")
-    public String actualizar(@PathVariable int id, @RequestParam("nombre") String nombre, @RequestParam("nombreUsuario") String nombreUsuario, 
-            @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("telefono") long telefono, 
+    public String actualizar(@PathVariable int id, @RequestParam("nombre") String nombre, @RequestParam("nombreUsuario") String nombreUsuario,
+            @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("telefono") long telefono,
             @RequestParam("archivo") MultipartFile archivo) throws Exception {
-        try{
+        try {
             usuarioServicio.actualizar(id, nombre, nombreUsuario, email, password, telefono, archivo);
             return "Exito";
         } catch (Exception exception) {
@@ -55,13 +62,13 @@ public class UsuarioControlador {
     }
 
     @GetMapping("/listar")
-    public String listar(ModelMap modelo){
+    public String listar(ModelMap modelo) {
         List<Usuario> usuarios = usuarioServicio.listarUsuarios();
         modelo.addAttribute("usuario", usuarios);
-        
+
         return "usuarioList";
     }
-    
+
     @DeleteMapping("/borrar/{id}")
     public String borrarUsuario(@PathVariable Integer id) throws Exception {
         try {
@@ -72,6 +79,11 @@ public class UsuarioControlador {
             return "Error";
         }
 
+    }
+
+    @GetMapping("/terminos")
+    public String terminos() {
+        return "terminos";
     }
 
 }
