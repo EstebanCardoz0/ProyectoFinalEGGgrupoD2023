e33b37f79bf242b109c71909e0af77486c3bb17c
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2023-03-14 21:48:48 -0300

    creacion del crud cliente

---
+++ QuinchApp/src/main/java/com/QuinchApp/Servicios/ClienteServicio.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.QuinchApp.Servicios;
+
+import com.QuinchApp.Entidades.Cliente;
+import com.QuinchApp.Entidades.Imagen;
+import com.QuinchApp.Repositorios.ClienteRepositorio;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author Esteban
+ */
+@Service
+public class ClienteServicio {
+
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+
+    @Autowired
+    private ImagenServicio imagenServicio;
+
+    @Transactional
+    public void registrar(String nombre, String nombreCliente, String email, String password, String password2, long telefono, MultipartFile archivo) throws Exception {
+        validar(nombre, nombreCliente, email, password, telefono, archivo, password2);
+        Cliente cliente = new Cliente();
+        cliente.setNombre(nombre);
+        cliente.setNombreUsuario(nombreCliente);
+        cliente.setEmail(email);
+        //usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+        cliente.setPassword(password);
+        // cliente.setRol(Rol.USER);
+        cliente.setTelefono(telefono);
+        Date fechaAlta = new Date();
+        cliente.setFechaAlta(fechaAlta);
+        boolean activo = Boolean.TRUE;
+        cliente.setActivo(activo);
+        Imagen miImagen = imagenServicio.guardar(archivo);
+        cliente.setFotoPerfil(miImagen);
+        clienteRepositorio.save(cliente);
+    }
+
+    //poner reserva
+    @Transactional
+    public void actualizar(int id, String nombre, String nombreCliente, String email, String password, long telefono, MultipartFile archivo) throws Exception {
+        if (id < 0) {
+            throw new Exception("Ingrese un id");
+        }
+        Optional<Cliente> respuesta = clienteRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Cliente usuario = respuesta.get();
+            usuario.setNombre(nombre);
+            usuario.setNombreUsuario(nombreCliente);
+            usuario.setEmail(email);
+            usuario.setPassword(password);
+            usuario.setTelefono(telefono);
+            int idImagen = 0;
+            if (usuario.getFotoPerfil() != null) {
+                idImagen = usuario.getFotoPerfil().getIdImagen();
+            }
+            Imagen miImagen = imagenServicio.actualizar(archivo, idImagen);
+            usuario.setFotoPerfil(miImagen);
+            clienteRepositorio.save(usuario);
+        }
+    }
+
+    public List<Cliente> listarClientes() {
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        return clientes;
+    }
+
+    @Transactional
+    public void borrar(Integer id) {
+        clienteRepositorio.deleteById(id);
+    }
+
+    //sumar reserva
+    private void validar(String nombre, String nombreCliente, String email, String password, long telefono, MultipartFile archivo, String password2) throws Exception {
+        if (nombre.isEmpty() || nombre == null) {
+            throw new Exception("El nombre no puede estar estar vacío");
+        }
+        if (nombreCliente.isEmpty() || nombreCliente == null) {
+            throw new Exception("El nombre del cliente no puede estar vacío");
+        }
+        if (email.isEmpty() || email == null) {
+            throw new Exception("El email no puede estar vacio");
+        }
+        if (password.isEmpty()) {
+            throw new Exception("La contraseña no puede estar vacía");
+        }
+        if (password2.isEmpty()) {
+            throw new Exception("Debe repetir la contraseña");
+        }
+        if (!password.equals(password2)) {
+            throw new Exception("Las contraseñas ingresadas deben ser iguales");
+        }
+        if (telefono == 0L) {
+            throw new Exception("El telefono no puede estar vacío");
+        }
+        if (archivo == null) {
+            throw new Exception("La imagen no puede estar vacía");
+        }
+        Cliente cliente = clienteRepositorio.buscarPorEmail(email);
+        if (cliente != null) {
+            throw new Exception("El email ya se encuentra registrado, pruebe con otro");
+        }
+    }
+
+}
