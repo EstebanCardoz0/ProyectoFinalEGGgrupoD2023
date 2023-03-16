55941be680388d7728392cb64fe677eef6fcc496
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-03-16 11:53:31 -0300

    reserva

--- QuinchApp/src/main/java/com/QuinchApp/Controladores/UsuarioControlador.java
+++ QuinchApp/src/main/java/com/QuinchApp/Controladores/UsuarioControlador.java
@@ -65,7 +65,6 @@ public class UsuarioControlador {
     public String listar(ModelMap modelo) {
         List<Usuario> usuarios = usuarioServicio.listarUsuarios();
         modelo.addAttribute("usuario", usuarios);
-
         return "usuarioList";
     }
 
