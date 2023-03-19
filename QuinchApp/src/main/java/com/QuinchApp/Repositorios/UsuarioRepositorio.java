63d044a6352f6594b3c3d7237833ff9b23869779
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-03-19 19:19:47 -0300

    agregar propiedad y registro segun rol

--- QuinchApp/src/main/java/com/QuinchApp/Repositorios/UsuarioRepositorio.java
+++ QuinchApp/src/main/java/com/QuinchApp/Repositorios/UsuarioRepositorio.java
@@ -1,6 +1,7 @@
 package com.QuinchApp.Repositorios;
 
 import com.QuinchApp.Entidades.Usuario;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +14,7 @@ public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
     public Usuario buscarPorEmail(@Param("email") String email);
 
     @Query("SELECT u FROM Usuario u WHERE u.nombreUsuario = :nombreUsuario")
-    public Usuario buscarPorNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
+    public Optional<Usuario> buscarPorNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
 
     @Query("SELECT u FROM Usuario u WHERE u.nombre = :nombre")
     public Usuario buscarPorNombre(@Param("nombre") String nombre);
